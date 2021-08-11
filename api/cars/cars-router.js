148c9554addd7d44ada3a0ad511af291cb1d7fa5
Matt Phillips <mattcphillips17@gmail.com>
2021-08-10 18:49:14 -0700

    getAll model func wired

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -6,15 +6,30 @@ const router = express.Router()
 
 
 router.get('/', (req, res, next) => {
-
+  Cars.getAll()
+    .then(cars => {
+      if (!cars) {
+        res.send([])
+      } else {
+        res.status(200).json(cars)
+      }
+    })
+    .catch(next)
 })
 
 router.get('/:id', (req, res, next) => {
-  
+  Cars.getById(req.params.id)
 })
 
 router.get('/', (req, res, next) => {
-  
+  Cars.create(req.body)
+})
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
 })
 
 
